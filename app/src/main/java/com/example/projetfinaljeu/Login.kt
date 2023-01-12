e564612ab9445cbc017084d6262b84c81d4f0bcb
Richbell <apovorichbell2018@gmail.com>
2023-01-12 08:55:00 +0100

    Commit

--- app/src/main/java/com/example/projetfinaljeu/Login.kt
+++ app/src/main/java/com/example/projetfinaljeu/Login.kt
@@ -1,11 +1,10 @@
 package com.example.projetfinaljeu
 
 import android.os.Parcelable
-import android.provider.ContactsContract.CommonDataKinds.Email
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Login (
-    val email: Email,
+    val email: String,
     val password: String
     )  : Parcelable
