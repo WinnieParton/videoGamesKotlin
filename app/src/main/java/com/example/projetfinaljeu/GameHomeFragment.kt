954c36432ee6b82227eb9db483552de8be507be9
Winnie Koum <wkoumwonkoagni@myges.fr>
2023-01-12 20:06:05 +0100

    update projet

--- app/src/main/java/com/example/projetfinaljeu/GameHomeFragment.kt
+++ app/src/main/java/com/example/projetfinaljeu/GameHomeFragment.kt
@@ -5,13 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projetfinaljeu.databinding.FragmentGameHomeBinding
+import kotlinx.android.synthetic.main.fragment_game_home.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
-class GameHomeFragment : Fragment() {
-
+class GameHomeFragment : Fragment(R.layout.fragment_game_home) {
+   // private val login: GameHomeFragmentArgs by navArgs()
+   lateinit var rv:RecyclerView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+       // System.out.println("infos de connexion "+login)
     }
 
     override fun onCreateView(
@@ -22,5 +33,39 @@ class GameHomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_game_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getGame();
+       /* GlobalScope.launch(Dispatchers.Default) {
 
+            val response = ApiClient.getGames()
+
+            withContext(Dispatchers.Main) {
+                getGame(response);
+            }
+        }*/
+    }
+
+    private fun getGame() {
+//response: ServerResponse
+        val games: List<Game> = listOf(Game(1),
+            Game(6),
+            Game(8),Game(6),
+            Game(8),Game(6),
+            Game(8))
+  rv = list_game_recyclerview
+        //scroller ver le haut
+        rv.layoutManager = LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, true)
+        //scroller vers le bas
+        rv.layoutManager = LinearLayoutManager(getContext())
+        rv.adapter = GamesAdapter(games, listener)
+
+    }
+    private val listener = GamesAdapter.OnClickListener { game ->
+        // Add action to navigate
+        findNavController().navigate(
+            GameHomeFragmentDirections.actionGameHomeFragmentToGameDetailFragment(game)
+        )
+
+    }
 }
--- app/src/main/java/com/example/projetfinaljeu/MainActivity.kt
+++ app/src/main/java/com/example/projetfinaljeu/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.projetfinaljeu
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import android.os.Bundle
-import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import kotlinx.android.synthetic.main.loader.*
+import kotlinx.android.synthetic.main.item_game_list.*
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         //progressBar.visibility=View.VISIBLE
         //pour rendre invisible
        // progressBar.visibility=View.GONE
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
